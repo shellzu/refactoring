0a7b67ecb08d8ada9163014b7d19b45039904cac
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-12 19:30:33 +0900

    料金計算を行う関数を抽出

--- src/main.js
+++ src/main.js
@@ -8,25 +8,7 @@ module.exports = function statement (invoice, plays) {
     
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-        case "tragedy":
-            thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
-            }
-            thisAmount += 300 * perf.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience -30, 0);
@@ -36,6 +18,28 @@ module.exports = function statement (invoice, plays) {
         result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
+    function amountFor(perf, play) {
+        let thisAmount = 0;
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return thisAmount;
+    }
+
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
