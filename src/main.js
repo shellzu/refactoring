aeb01a4cf19285740ff7f68b04ac106c819c5d29
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-12 19:56:55 +0900

    一つ前のコミットメッセージは間違い。ここでplayパラメータの削除

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ module.exports = function statement (invoice, plays) {
                         minimumFractionDigits: 2 }).format;
     
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience -30, 0);
@@ -17,7 +17,7 @@ module.exports = function statement (invoice, plays) {
         result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
         switch (playFor(aPerformance).type) {
             case "tragedy":
