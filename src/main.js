078faf920add328621de37dd64471e103506e65e
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-13 23:23:51 +0900

    変数のインライン化（totalVolumeCredits）

--- src/main.js
+++ src/main.js
@@ -6,9 +6,8 @@ module.exports = function statement (invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    let volumeCredits = totalVolumeCredits();
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function amountFor(aPerformance) {
