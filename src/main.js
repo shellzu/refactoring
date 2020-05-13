668fb4fcd114868f0a620f6169180f3a904b015f
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-13 22:51:11 +0900

    ポイント計算関数の引数と返り値の変数に役割を持たせた

--- src/main.js
+++ src/main.js
@@ -39,11 +39,11 @@ module.exports = function statement (invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function volumeCreditsFor(perf) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience -30, 0);
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
-        return volumeCredits;
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience -30, 0);
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+        return result;
     }
 
     result += `Amount owed is ${format(totalAmount/100)}\n`;
