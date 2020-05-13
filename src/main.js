e0b9308e9bcb643ab0f05498550cbe0498ea0059
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-13 23:15:54 +0900

    ステートメントのスライド（volumeCredits）

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 module.exports = function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // 注文の内訳を出力
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-
+    let volumeCredits = 0;
     for(let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
