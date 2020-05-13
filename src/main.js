1fd67b87eec5d1cacc05429d86474dbd1061fd17
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-13 23:13:06 +0900

    ボリューム特典ポイント集計ループの分離

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ module.exports = function statement (invoice, plays) {
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // 注文の内訳を出力
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
+    }
+
+    for(let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
