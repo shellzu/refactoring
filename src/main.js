0e5d934359686618ff259b3d4f4416b82e68eae7
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-14 22:03:56 +0900

    請求書印刷関数としてstatementから抽出

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-module.exports = function statement (invoice, plays) {
+function statement (invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+module.exports = function renderPlainText (invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // 注文の内訳を出力
@@ -62,4 +66,5 @@ module.exports = function statement (invoice, plays) {
         }
         return result;
     }
+    
 }
