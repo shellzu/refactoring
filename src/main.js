ff5482b62f8fcd1758425eb8b8c2cdf82c22c392
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-18 12:46:08 +0900

    data経由で演劇（play）を取得するように変更

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ function renderPlainText (data, invoice, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         // 注文の内訳を出力
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -27,7 +27,7 @@ function renderPlainText (data, invoice, plays) {
 
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -42,19 +42,15 @@ function renderPlainText (data, invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
     }
 
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         result += Math.max(aPerformance.audience -30, 0);
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5)
         return result;
     }
 
