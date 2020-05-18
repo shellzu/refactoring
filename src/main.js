ac6b00c83b13693f872edf7d397bff88b0851bc2
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-18 20:21:08 +0900

    関数の移動（amountFor）

--- src/main.js
+++ src/main.js
@@ -7,23 +7,9 @@ module.exports = function statement (invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-}
-
-function renderPlainText (data, invoice, plays) {
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        // 注文の内訳を出力
-        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
 
     function amountFor(aPerformance) {
         let result = 0;
@@ -46,6 +32,21 @@ function renderPlainText (data, invoice, plays) {
         }
         return result;
     }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+}
+
+function renderPlainText (data, invoice, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        // 注文の内訳を出力
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
 
     function volumeCreditsFor(aPerformance) {
         let result = 0;
@@ -71,7 +72,7 @@ function renderPlainText (data, invoice, plays) {
     function totalAmount() {
         let result = 0;
         for(let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
