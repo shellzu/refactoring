d061e1b97a876045981067f697985240f9cab45d
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-25 22:55:24 +0900

    出力フェーズ（statement関数）の抽出

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 module.exports = function statement (invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // 注文の内訳を出力
@@ -62,4 +66,4 @@ module.exports = function statement (invoice, plays) {
         }
         return result;
     }
-} 
+}
