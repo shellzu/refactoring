3a7965b2967a535343f9e723961cef46f6a08662
貝津志織 <kaizushiori@kaitsushiorinoMacBook-Air.local>
2020-05-27 22:31:41 +0900

    createStatementDataで適切なサブクラスを使う

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -7,7 +7,7 @@ module.exports = function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
@@ -34,6 +34,10 @@ module.exports = function createStatementData(invoice, plays) {
         }
         return volumeCredits;
     }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 class PerformanceCalculator {
